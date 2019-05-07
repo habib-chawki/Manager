39db19f5ac2cd8a5375d7e4b15f13875544373bb
habib-chawki <touati.habib.chawki@gmail.com>
2019-05-07 19:33:53 +0200

    Added delete button functionality

--- app/src/main/java/com/android/stockmanager/ProductListAdapter.java
+++ app/src/main/java/com/android/stockmanager/ProductListAdapter.java
@@ -39,12 +39,12 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
         String current = products.get(i);
         productViewHolder.productItemView.setText(current);
 
-        //set a click listener for the three-dotted imagebutton
-        productViewHolder.btn.setOnClickListener(new View.OnClickListener() {
+        //set a click listener for the three-dotted image-button
+        productViewHolder.popupMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //create the popupMenu and anchor it to the current button
-                popupMenu = new PopupMenu(productViewHolder.itemView.getContext(), productViewHolder.btn);
+                popupMenu = new PopupMenu(productViewHolder.itemView.getContext(), productViewHolder.popupMenuButton);
                 MenuInflater inflater = popupMenu.getMenuInflater();
                 inflater.inflate(R.menu.menu_actions, popupMenu.getMenu());
                 popupMenu.show();
@@ -53,14 +53,28 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
                 popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
                     @Override
                     public boolean onMenuItemClick(MenuItem menuItem) {
-                        int pos = productViewHolder.getAdapterPosition();
-                        Toast.makeText(productViewHolder.itemView.getContext(), menuItem.getTitle() + " " + pos, Toast.LENGTH_SHORT).show();
+                        int productPosition = productViewHolder.getAdapterPosition();
+                        //check which button was clicked
+                        switch(menuItem.getItemId()){
+                            //delete a product from the list
+                            case R.id.menu_item_delete:
+                                products.remove(productPosition);
+                                notifyItemRemoved(productPosition);
+                                break;
+                                //show the details of a given product
+                            case R.id.menu_item_details:
+                                Toast.makeText(productViewHolder.itemView.getContext(), "Details Clicked " + productPosition, Toast.LENGTH_SHORT).show();
+                                break;
+                                //edit the information of a product
+                            case R.id.menu_item_edit:
+                                Toast.makeText(productViewHolder.itemView.getContext(), "Edit Clicked " + productPosition, Toast.LENGTH_SHORT).show();
+                                break;
+                        }
                         return true;
                     }
                 });
             }
         });
-
     }
 
     @Override
@@ -69,16 +83,14 @@ public class ProductListAdapter extends RecyclerView.Adapter<ProductListAdapter.
     }
 
 
-
-
     public static class ProductViewHolder extends RecyclerView.ViewHolder{
         public TextView productItemView;
-        public ImageButton btn;
+        public ImageButton popupMenuButton;
 
         public ProductViewHolder(@NonNull View itemView) {
             super(itemView);
             productItemView = itemView.findViewById(R.id.product);
-            btn = itemView.findViewById(R.id.imgbutton_actions);
+            popupMenuButton = itemView.findViewById(R.id.imgbutton_actions);
         }
 
     }
